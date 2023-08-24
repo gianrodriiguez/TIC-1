f4ca2548335e5f844ead153dfdb3a4f2c8545689
gianrodriiguez <gianrodr@gmail.com>
2023-08-24 12:53:55 -0300

    Base hecha

---
+++ src.main.java/src/DemoApp.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class DemoApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("JavaFX Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
